966b5cb76a36a35fa14830f88bf5f2b2dfb29773
Nicolas Riquelme Guzman <nrriquelme@gmail.com>
2018-03-21 12:01:32 -0300

    adding login and admin button to show navigation to the other sections

--- src/components/navegacion.js
+++ src/components/navegacion.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 class Navegacion extends Component {
   render() {
@@ -17,6 +18,16 @@ class Navegacion extends Component {
           </NavItem>
           <NavItem>
             <NavLink href="#">Frutas</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="btn border-warning">
+              <Link to="/login">Login</Link>
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="btn border-warning" style={{marginLeft: "5px"}}>
+              <Link to="/admin">Admin</Link>
+            </NavLink>
           </NavItem>
         </Nav>
       </div>
